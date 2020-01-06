502e07436e66ed222cd470c2b0419e8b13979f76
JacksonWakefield <55509800+JacksonWakefield@users.noreply.github.com>
2020-01-05 18:46:43 -0700

    dded in the bar with the text n stuff yeah

--- codingimagined/src/App.js
+++ codingimagined/src/App.js
@@ -1,13 +1,26 @@
-import React from 'react';
-import logo from './codinglogo.svg';
-import './App.css';
+import React from "react";
+import logo from "./codinglogo.svg";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        
+        <div class="buttons">
+          <p className="Header-Button" tag="sample1">
+            Explore
+          </p>
+          <p className="Header-Button" tag="sample2">
+            About us
+          </p>
+          <p className="Header-Button" tag="sample3">
+            More Stuff
+          </p>
+          <p className="Header-Button" tag="sample4">
+            Even More Stuff
+          </p>
+        </div>
       </header>
     </div>
   );
