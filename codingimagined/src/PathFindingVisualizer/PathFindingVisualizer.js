b2fff947fc4b82a7e288e2140fd14eb1ba6a621f
JacksonWakefield <55509800+JacksonWakefield@users.noreply.github.com>
2020-01-12 16:25:39 -0700

    hing the colors woooo

--- codingimagined/src/PathFindingVisualizer/PathFindingVisualizer.js
+++ codingimagined/src/PathFindingVisualizer/PathFindingVisualizer.js
@@ -1,55 +1,50 @@
-import React, {Component} from'react';
+import React, { Component } from "react";
 import Node from "./Node";
-export default class PathFindingVisualizer extends Component { 
-    constructor(props) {
-        super(props);
-        this.state = {
-            isLoading: true,
-            grid: null
-        }
-    }
-    componentDidMount() {
-        let width = 10;
-        let height = 10;
-        var grid = [];
-        for (let i = 0; i < width; i++) {
-            const currentRow = []
-            for (let j = 0; j < height; j ++) {
-                currentRow.push({
-                    col: i,
-                    row: j
-                });
-            }
-            grid.push(currentRow);
-        }
-        this.setState({
-            isLoading: false,
-            grid: grid
+export default class PathFindingVisualizer extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isLoading: true,
+      grid: null
+    };
+  }
+  componentDidMount() {
+    let width = 10;
+    let height = 10;
+    var grid = [];
+    for (let i = 0; i < width; i++) {
+      const currentRow = [];
+      for (let j = 0; j < height; j++) {
+        currentRow.push({
+          col: i,
+          row: j
         });
+      }
+      grid.push(currentRow);
     }
-    render() {
-        if(this.state.isLoading) {
-            return(
-                <div></div>
-            );
-        } else {
-            let grid = this.state.grid;
+    this.setState({
+      isLoading: false,
+      grid: grid
+    });
+  }
+  render() {
+    if (this.state.isLoading) {
+      return <div></div>;
+    } else {
+      let grid = this.state.grid;
+      return (
+        <div className="center">
+          {grid.map(row => {
             return (
-                <div className="center">
-                   {grid.map(row => {
-                        return (
-                            <div> 
-                                {row.map(node => {
-                                    return(
-                                        <Node col={node.col} row={node.row}></Node>
-                                );
-                            })}
-                            </div>
-                        );
-                    }
-                   )}
-                </div>
-            );  
-        }
+              <div>
+                {row.map(node => {
+                  return <Node col={node.col} row={node.row}></Node>;
+                })}
+              </div>
+            );
+          })}
+        </div>
+      );
     }
+  }
 }
