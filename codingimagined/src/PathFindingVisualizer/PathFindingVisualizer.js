ef3637dd939ceed349eacb2d046638a71a8b1342
Mudit Gupta <muditg317@gmail.com>
2020-01-28 00:06:54 -0500

    fix pathfinding visualizer

--- codingimagined/src/PathFindingVisualizer/PathFindingVisualizer.js
+++ codingimagined/src/PathFindingVisualizer/PathFindingVisualizer.js
@@ -14,28 +14,28 @@ export default class PathFindingVisualizer extends Component {
       mouseDown: false,
       currentSelection: "1"
     };
-    this.testD();
+    // this.testD();
   }
 
-  testD() {
-    const link = 'https://oz4akoxz8g.execute-api.us-east-2.amazonaws.com/Testing/';
-    let data = {
-      method: 'POST',
-      mode: 'no-cors',
-      headers: {
-          'Content-Type': 'application/json',
-      },
-      body: JSON.stringify({
-          'grid':this.state.grid
-      })
-      }
-      fetch(link, data)
-      .then(response => response.json())  // promise // lmao
-      .then(response => {
-          //console.log(response);
-          //console.log(response['body'][0]);
-      })
-  }
+  // testD() {
+  //   const link = 'https://oz4akoxz8g.execute-api.us-east-2.amazonaws.com/Testing/';
+  //   let data = {
+  //     method: 'POST',
+  //     mode: 'no-cors',
+  //     headers: {
+  //         'Content-Type': 'application/json',
+  //     },
+  //     body: JSON.stringify({
+  //         'grid':this.state.grid
+  //     })
+  //     }
+  //     fetch(link, data)
+  //     .then(response => response.json())  // promise // lmao
+  //     .then(response => {
+  //         //console.log(response);
+  //         //console.log(response['body'][0]);
+  //     })
+  // }
 
   createGrid() {
     let width = 15;
@@ -54,12 +54,12 @@ export default class PathFindingVisualizer extends Component {
           nodeState = "start";
         } else if (i === endNode.col && j === endNode.row) {
           nodeState = "end";
-        } 
+        }
         currentRow.push({
           col: i,
           row: j,
           id: id,
-          state: nodeState, 
+          state: nodeState,
           weightvalue: 0
         });
         id++;
@@ -76,16 +76,16 @@ export default class PathFindingVisualizer extends Component {
   addNode(row, col) {
     if(this.state.mouseDown) {
       if(this.state.grid[col][row].state === "expand" || this.state.grid[col][row].state === "weight") {
-        this.setState({grid: update(this.state.grid, {[col]: {[row]: {state: {$set: "none"}}}})}); 
+        this.setState({grid: update(this.state.grid, {[col]: {[row]: {state: {$set: "none"}}}})});
       } else if(this.state.grid[col][row].state === "end") {
 
       } else if(this.state.grid[col][row].state === "start") {
 
       } else {
         if(this.state.currentSelection === "1") {
-          this.setState({grid: update(this.state.grid, {[col]: {[row]: {state: {$set: "expand"}}}})}); 
+          this.setState({grid: update(this.state.grid, {[col]: {[row]: {state: {$set: "expand"}}}})});
         } else if(this.state.currentSelection === "2") {
-          this.setState({grid: update(this.state.grid, {[col]: {[row]: {state: {$set: "wall"}}}})}); 
+          this.setState({grid: update(this.state.grid, {[col]: {[row]: {state: {$set: "wall"}}}})});
         }
       }
     }
@@ -93,16 +93,16 @@ export default class PathFindingVisualizer extends Component {
 
   handleMouseDown(row, col) {
     if(this.state.grid[col][row].state === "expand" || this.state.grid[col][row].state === "weight") {
-      this.setState({grid: update(this.state.grid, {[col]: {[row]: {state: {$set: "none"}}}})}); 
+      this.setState({grid: update(this.state.grid, {[col]: {[row]: {state: {$set: "none"}}}})});
     } else if(this.state.grid[col][row].state === "end") {
 
     } else if(this.state.grid[col][row].state === "start") {
 
     } else {
       if(this.state.currentSelection === "1") {
-        this.setState({grid: update(this.state.grid, {[col]: {[row]: {state: {$set: "expand"}}}})}); 
+        this.setState({grid: update(this.state.grid, {[col]: {[row]: {state: {$set: "expand"}}}})});
       } else if (this.state.currentSelection === "2") {
-        this.setState({grid: update(this.state.grid, {[col]: {[row]: {state: {$set: "wall"}}}})}); 
+        this.setState({grid: update(this.state.grid, {[col]: {[row]: {state: {$set: "wall"}}}})});
       }
     }
     this.setState({mouseDown: true});
@@ -118,7 +118,7 @@ export default class PathFindingVisualizer extends Component {
       grid: this.createGrid()
     });
   }
-  
+
 
 
   render() {
@@ -146,13 +146,13 @@ export default class PathFindingVisualizer extends Component {
               <div key={row[0].col} className="row">
                 {row.map(node => {
                   return <Node
-                    col={node.col} 
-                    row={node.row} 
+                    col={node.col}
+                    row={node.row}
                     key={node.id}
                     addNode={(row, col) => this.addNode(node.row, node.col)}
                     handleMouseDown={(row, col) => this.handleMouseDown(node.row, node.col)}
                     handleMouseUp={() => this.handleMouseUp()}
-                    state={node.state}  
+                    state={node.state}
                     ></Node>;
                 })}
               </div>
