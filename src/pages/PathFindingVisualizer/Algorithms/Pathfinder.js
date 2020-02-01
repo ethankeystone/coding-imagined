e81af8b988d89ffdbc800ae5ef8753f8201759ec
JacksonWakefield <55509800+JacksonWakefield@users.noreply.github.com>
2020-02-01 15:01:45 -0700

    Adding object oriented

---
+++ src/pages/PathFindingVisualizer/Algorithms/Pathfinder.js
@@ -0,0 +1,7 @@
+export default class Pathfinder{
+    constructor(grid, startNode, endNode){
+        this.grid = grid;
+        this.startNode = startNode;
+        this.endNode = endNode;
+    }
+}
